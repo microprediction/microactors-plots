35a3a03823777df1bf38bf1718fe3e4462ede8af
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2020-12-21 09:59:30 -0500

    Update fit.py

--- fit.py
+++ fit.py
@@ -7,48 +7,54 @@ import time
 from pprint import pprint
 import warnings
 warnings.filterwarnings('ignore')
-from copulas.multivariate import GaussianMultivariate
+from copulas.multivariate import VineCopula
 import pandas as pd
 
 
-# Grab the Github secrets
+# 1. Grab the Github secrets
 # For this script to work you need to create four separate GitHub secrets
 # called WRITE_KEY_1 WRITE_KEY_2 WRITE_KEY_3 and WRITE_KEY_4
+# The idea is that this way you get 900 samples instead of 225
 import os 
-WRITE_KEYS = [ os.environ.get('WRITE_KEY_'+str(i+1)) for i in range(4) ]      
-
-
+WRITE_KEYS = [ os.environ.get('WRITE_KEY_'+str(i+1)) for i in range(4) ]   
 print('Copula syndicate is firing up.')
 for write_key in WRITE_KEYS:
     animal = MicroWriter.animal_from_key(write_key)  
     print(animal)
     
+    
+# 2. Pick a copulas
+VINES = ['center','regular','direct']. # See https://sdv.dev/Copulas/tutorials/03_Multivariate_Distributions.html#Vine-Copulas
+VINE_TYPE = VINES[0]                        # <----- Maybe you want to try something else
+
+# 3. (Optional) Set the URL of your repo so that others can learn from it     
 REPO = 'https://github.com/microprediction/microactors-plots/blob/master/fit.py'  # <--- Change your username
 
 
-# Get historical data, fit a copula, and submit 
-
+# 4. Create your fitting function 
 def fit_and_sample(lagged_zvalues:[[float]],num:int, copula=None):
     """ Example of fitting a copula function, and sampling
-           lagged_zvalues: [ [z1,z2,z3] ]  distributed N(0,1) margins, roughly
-           copula : Something from https://pypi.org/project/copulas/
+    
+           lagged_zvalues: [ [z1,z2,z3] ]  Data with roughly N(0,1) margins
+           copula : 
            returns: [ [z1, z2, z3] ]  representative sample
+           
     """
-    # This is the part you'll want to change. 
+    # This is the part where there's plenty of room for improvement 
     # Remark 1: It's lazy to just sample synthetic data
-    # Some more evenly spaced sampling would be preferable. 
+    #           Some more evenly spaced sampling would be preferable. 
+    #           See https://www.microprediction.com/blog/lottery for discussion of why evenly 
+    #           spaced samples are likely to serve you better. 
     # Remark 2: Any multivariate density estimation could go here. 
     # Remark 3: If you want to literally fit to a Copula (i.e. roughly uniform margins)
     # then you might want to use mw.get_lagged_copulas(name=name, count= 5000) instead
-    #
-    # See https://www.microprediction.com/blog/lottery for discussion of why evenly 
-    # spaced samples are likely to serve you better. 
+    
 
     df = pd.DataFrame(data=lagged_zvalues)
     if copula is None:
-        copula = GaussianMultivariate() # <--- Change the Copula to something else?
+        copula = VineCopula(VINE_TYPE) 
     copula.fit(df)
-    synthetic = copula.sample(4*num)
+    synthetic = copula.sample(4*num)  # Again, see remarks above
     return synthetic.values.tolist()
 
 
@@ -67,6 +73,8 @@ if __name__ == "__main__":
                 zvalues = fit_and_sample(lagged_zvalues=lagged_zvalues, num=num*four)
                 pprint((name, delay))
                 try:
+                    # Split the samples up amongst the syndicate
+                    # This would be more effective if the samples were not random :-)
                     responses = list()
                     for j, write_key = enumerate(WRITE_KEYS)
                         zvalues_j = zvalues[j*num:(j+1)*num]
