0e0176984c55713e8263bcafcfae667ccfb71786
Peter Cotton <peter.cotton@microprediction.com>
2020-12-21 23:48:09 -0500

    priv

--- config_private.py
+++ config_private.py
@@ -1,9 +1,4 @@
 
 import os
 
-os.environ['WRITE_KEY_1']='baf7e5692237cc98b10dd9646b464bf0'
-os.environ['WRITE_KEY_2']='f8ca8202168445c9f42b8618a06bbe1a'
-os.environ['WRITE_KEY_3']='028f5b439d75ac1ca02c59ed63e12b7d'
-os.environ['WRITE_KEY_4']='a6c968a9b1f16693405353231e8f1488'
-
 NOTHING=''
